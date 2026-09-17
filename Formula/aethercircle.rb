e36285c9b8cee928de55b08ab6a465e43ec2470c
Steve Sheets <magesteve@mac.com>
2026-09-17 12:38:55 -0400

    Use GitHub SSH over port 443

--- Formula/aethercircle.rb
+++ Formula/aethercircle.rb
@@ -1,7 +1,7 @@
 class Aethercircle < Formula
   desc "Cross-platform AetherCircle immersive application development tools"
   homepage "https://aethercircle.org"
-  head "ssh://git@github.com/Zodiac-Innovations/AetherCircleCLI.git", branch: "main"
+  head "ssh://git@ssh.github.com:443/Zodiac-Innovations/AetherCircleCLI.git", branch: "main"
 
   depends_on :macos
   depends_on "xcodegen"
